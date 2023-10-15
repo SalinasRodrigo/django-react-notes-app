25284696b7f6368599a915f3c235027e3f9fcc9b
Rodrigo Salinas <salinas.e.rodrigo@gmail.com>
2023-10-14 22:01:04 -0300

    modelos creados y configuración de metodos GET para la API

--- server/noteProject/api/urls.py
+++ server/noteProject/api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', view=views.getRoutes, name="routes")
+  path('', view=views.getRoutes, name="routes"),
+  path('notes/', view=views.getNotes, name="notes"),
+  path('notes/<int:id>/', view=views.getNote, name="note"),
 ]
--- server/noteProject/api/views.py
+++ server/noteProject/api/views.py
@@ -1,9 +1,53 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
 
-
+@api_view(['GET'])
 def getRoutes(request):
-    return JsonResponse("Our API", safe=False)
+    routes = [
+        {
+            "Endpoint": "/notes/",
+            "method": "GET",
+            "body": None,
+            "description": "Returns an array of notes",
+        },
+        {
+            "Endpoint": "/notes/id",
+            "method": "GET",
+            "body": None,
+            "description": "Returns a single note object",
+        },
+        {
+            "Endpoint": "/notes/create/",
+            "method": "POST",
+            "body": {"body": ""},
+            "description": "Creates new note with data sent in post request",
+        },
+        {
+            "Endpoint": "/notes/id/update/",
+            "method": "PUT",
+            "body": {"body": ""},
+            "description": "Creates an existing note with data sent in post request",
+        },
+        {
+            "Endpoint": "/notes/id/delete/",
+            "method": "DELETE",
+            "body": None,
+            "description": "Deletes and exiting note",
+        },
+    ]
+    return Response(routes)
 
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serialNotes = NoteSerializer(notes, many=True)
+    return Response(serialNotes.data)
 
-# Create your views here.
+@api_view(['GET'])
+def getNote(request, id):
+    notes = Note.objects.get(id=id)
+    serialNotes = NoteSerializer(notes, many=False)
+    return Response(serialNotes.data)
